e66c52fb2d8d4578037ff452bca7fd3f4bb45753
Serhii <serhii.furkalo@keyua.org>
2023-11-18 22:10:04 +0200

    f/g add get items in category and get item counts in categories

--- shop_service/app/routers/services.py
+++ shop_service/app/routers/services.py
@@ -1,11 +1,9 @@
 from typing import List, Annotated, Optional, Set, Dict
 from pydantic import BaseModel
 from fastapi import APIRouter, Depends, HTTPException, Query, Response
-from fastapi.responses import JSONResponse
-from sqlalchemy.orm import Session, aliased
-from sqlalchemy.sql import func, cast, select, text
+from sqlalchemy.orm import Session
+from sqlalchemy.sql import func, select
 from sqlalchemy.future import select
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import Base
 from app.database import SessionLocal
@@ -74,7 +72,7 @@ async def get_categories_for_items_api(db: db_dependency,
 
 
 @router.get("/items-in-category/{category_id}", response_model=List[ItemResponse])
-async def get_items_in_category(category_id: int, db=Depends(get_db)):
+async def get_items_in_category(db: db_dependency, category_id: int):
 
     items = {}
     stmt = (
@@ -88,3 +86,38 @@ async def get_items_in_category(category_id: int, db=Depends(get_db)):
     items = result.scalars().all()
 
     return items
+
+
+@router.get("/item-counts-in-categories/", response_model=dict)
+async def get_item_counts_in_categories(db: db_dependency, 
+                                  category_ids: List[int] = Query(..., description="List of category IDs")):
+    results = {}
+    for category_id in category_ids:
+        category = db.query(models.Category).get(category_id)
+        if category:
+            product_count = (
+                db.query(models.Item)
+                .join(models.ItemCategory)
+                .filter(models.ItemCategory.category_id == category.id)
+                .count()
+            )
+            results[category_id] = product_count
+        else:
+            results[category_id] = 0
+
+    return results
+
+
+@router.get("/total_unique_product_offers/")
+async def get_total_unique_product_offers(db: db_dependency,
+    category_ids: list[int] = Query(..., title="List of category IDs")):
+
+    total_unique_product_offers = (
+        db.query(models.Item)
+        .join(models.ItemCategory)
+        .filter(models.ItemCategory.category_id.in_(category_ids))
+        .distinct(models.Item.id)
+        .count()
+    )
+
+    return {"total_unique_product_offers": total_unique_product_offers}
